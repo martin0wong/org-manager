1468aafe891829a7a74473e33446f3d62718d4d7
Martin Wong <87554144+martin0wong@users.noreply.github.com>
2024-02-12 14:17:02 -0800

    code logic refactor

--- src/main/ui/OrgManager.java
+++ src/main/ui/OrgManager.java
@@ -12,7 +12,7 @@ public class OrgManager {
     private OrgEntity csss;
     private Scanner input;
     private OrgEntity currentOrg;
-    private SubEntity currentSubOrg;
+    private SubEntity currentSub;
 
     // EFFECTS: runs the teller application
     public OrgManager() {
@@ -75,7 +75,7 @@ public class OrgManager {
     // EFFECTS: displays menu of options to user
     private void displayMenu() {
         System.out.println("Current organization: " + currentOrg.getOrgName());
-        System.out.println("\nSelect from:");
+        System.out.println("Select from:");
         System.out.println("\ta -> add sub-entity");
         System.out.println("\tat -> add task to sub-entity");
         System.out.println("\tr -> remove sub-entity");
@@ -93,73 +93,67 @@ public class OrgManager {
         System.out.println("Enter new sub-entity headcount: ");
         int subHeadCount = input.nextInt();
 
-        SubEntity newSub = new SubEntity(subName,subHeadCount);
+        SubEntity newSub = new SubEntity(subName, subHeadCount);
 
         currentOrg.addSubEntity(newSub);
-        System.out.println(subName + "has been created as a sub-entity and added to list successfully!");
+        System.out.printf("%s has been created as a sub-entity and added to list successfully!%n", subName);
     }
 
     // MODIFIES: this
     // EFFECTS: create and add a new task for the sub-entity
     private void addTaskToSubEntity() {
-        currentSubOrg = selectSubEntity();
-        if (currentSubOrg != null) {
-            System.out.println("Enter new task");
+        currentSub = selectSubEntity();
+        if (currentSub != null) {
+            System.out.println("Enter new task:");
             String taskName = input.next();
 
             Task newTask = new Task(taskName);
 
-            currentSubOrg.addTask(newTask);
-            System.out.println(taskName + "has been created and assigned to " + currentSubOrg.getName());
+            currentSub.addTask(newTask);
+            System.out.printf("%s has been created and assigned to %s %n", taskName, currentSub.getName());
         }
     }
 
     // MODIFIES: this
     // EFFECTS: remove the sub-entity under the organization
     private void removeSubEntity() {
-        System.out.println("\nEnter sub-entity name: ");
-        String subName = input.next();
-        SubEntity sub = currentOrg.findSubEntity(subName);
-        if (sub != null) {
-            currentOrg.removeSubEntity(sub);
-        } else {
-            System.out.println("Sub-entity with given name cannot be found!");
+        currentSub = selectSubEntity();
+        if (currentSub != null) {
+            currentOrg.removeSubEntity(currentSub);
+            System.out.printf("%s removed from %s successfully!%n", currentSub.getName(), currentOrg.getOrgName());
         }
-
     }
 
     // EFFECT: see the headcount of selected sub-entity
-    private int seeSubHeadCount() {
-        System.out.println("Enter sub-entity name: ");
-        String subName = input.next();
+    private void seeSubHeadCount() {
+        currentSub = selectSubEntity();
 
-        if (currentOrg.findSubEntity(subName) != null) {
-            SubEntity sub = currentOrg.findSubEntity(subName);
-            return sub.getHeadCount();
-        } else {
-            System.out.println("Entered sub-entity name not found in " + currentOrg.getOrgName());
-            return -1;
+        if (currentSub != null) {
+            System.out.printf("%s has %s members. %n", currentSub.getName(), currentSub.getHeadCount());
         }
     }
 
-    // EFFECT: see the list of sub-entity under current organization
-    private void seeSubList() {
+    // EFFECT: see the list of sub-entity under current organization, returns true when list not empty, false otherwise
+    private boolean seeSubList() {
         LinkedList<SubEntity> list = currentOrg.getSubEntityList();
-        if (list.size() < 1) {
-            System.out.println("No sub-entity under current organization!");
+        if (list.size() == 0) {
+            System.out.println("No sub-entities under current organization!");
+            return false;
         } else {
+            System.out.printf("List of sub-entities under %s: %n", currentOrg.getOrgName());
             for (SubEntity i : list) {
-                System.out.println(i.getName());
+                System.out.println("\t" + i.getName());
             }
+            return true;
         }
 
     }
 
     // EFFECT: select organization to be viewed on manager
-    private OrgEntity selectOrgEntity() {
-        String selection = "";  // force entry into loop
+    private void selectOrgEntity() {
+        String selection = "";
 
-        while (! (selection.equals("sus") || selection.equals("csss"))) {
+        while (!(selection.equals("sus") || selection.equals("csss"))) {
             System.out.println("sus for Science Undergraduate Society");
             System.out.println("csss for Computer Science Student Society");
             selection = input.next();
@@ -168,29 +162,25 @@ public class OrgManager {
 
         if (selection.equals("sus")) {
             currentOrg = sus;
-            return sus;
         } else {
             currentOrg = csss;
-            return csss;
         }
     }
 
     // EFFECT: select sub-entity in the organization
     private SubEntity selectSubEntity() {
-        String selection = "";  // force entry into loop
-        LinkedList<SubEntity> list = currentOrg.getSubEntityList();
+        String selection = "";
+        if (seeSubList()) {
+            System.out.println("Enter name of sub-entity to select it: ");
+            selection = input.next();
 
-        for (SubEntity i : list) {
-            System.out.println(i.getName());
-        }
-
-        System.out.println("Enter name of sub-entity to select it: ");
-        selection = input.next();
-
-        if (currentOrg.findSubEntity(selection) != null) {
-            return currentOrg.findSubEntity(selection);
+            if (currentOrg.findSubEntity(selection) != null) {
+                return currentOrg.findSubEntity(selection);
+            } else {
+                System.out.printf("Entered sub-entity name not found in %s %n", currentOrg.getOrgName());
+                return null;
+            }
         } else {
-            System.out.println("Entered sub-entity name not found in " + currentOrg.getOrgName());
             return null;
         }
     }
