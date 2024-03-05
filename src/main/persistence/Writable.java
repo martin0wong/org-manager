370a38563bc401aeacf98032cf444a57efdd0240
UBC Student <tywong@students.cs.ubc.ca>
2024-03-05 13:45:04 -0800

    Citation

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,7 @@ package persistence;
 
 import org.json.JSONObject;
 
+// CITATION: similar to the demo given
 public interface Writable {
     // EFFECTS: returns this as JSON object
     JSONObject toJson();
